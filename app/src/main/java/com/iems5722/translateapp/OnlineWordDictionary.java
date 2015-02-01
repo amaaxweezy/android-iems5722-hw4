8bc6f88e396244a090043b2f8eead014580773b9
Mond Wan <mondwan.1015@gmail.com>
2015-02-01 16:33:22 +0800

    Implement Lookup dictionary via TCP
    --Overview
    1. Add INTERNET permission
    2. Convert Manifest to unix style (ending with \n)
    3. Add codes for showing server error if there are any

--- app/src/main/java/com/iems5722/translateapp/OnlineWordDictionary.java
+++ app/src/main/java/com/iems5722/translateapp/OnlineWordDictionary.java
@@ -4,6 +4,12 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.HashMap;
 
 public class OnlineWordDictionary extends AsyncTask<Void, Void, HashMap<String, String>> {
@@ -18,6 +24,12 @@ public class OnlineWordDictionary extends AsyncTask<Void, Void, HashMap<String, 
 
     // Store user input which will be looked up later
     protected String inputTxt = "";
+
+    // A boolean indicate there are any errors related to the server
+    protected boolean isServerError = false;
+
+    // An error message will be shown on dialog in case there are errors
+    protected String errorMessage = "";
 
     public OnlineWordDictionary(MainActivity mainActivity, String protocol, String inputTxt) {
         // Constructor of the OnlineWordDictionary
@@ -71,30 +83,74 @@ public class OnlineWordDictionary extends AsyncTask<Void, Void, HashMap<String, 
     protected HashMap<String, String> myTCPLookUp() {
         HashMap<String, String> ret = new HashMap<>();
 
-        ret.put("one", "一");
+        // Do TCP connection here
+        String inputTxt = this.inputTxt;
+
+        String host = "iems5722v.ie.cuhk.edu.hk";
+        int port = 3001;
+
+        try {
+            Socket s = new Socket(host, port);
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, String.format("Connect to |%s|", host));
+            }
+
+            DataOutputStream outs = new DataOutputStream(s.getOutputStream());
+
+            // Fill in true to flush output stream
+            PrintWriter myWriter = new PrintWriter(outs, true);
+            myWriter.println(this.inputTxt);
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, String.format("Send data |%s| to |%s|", inputTxt, host));
+            }
+
+            BufferedReader res = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String translatedTxt = res.readLine();
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, String.format("Received data |%s| from |%s|", translatedTxt, host));
+            }
+
+            // Make dictionary only if server translate successfully
+            if (!translatedTxt.matches(".*Error.*")) {
+                ret.put(inputTxt, translatedTxt);
+            }
+
+        } catch (IOException e) {
+            Log.e(TAG, String.format("Server error |%s|", e.getMessage()));
+
+            this.isServerError = true;
+            this.errorMessage = e.getMessage();
+        }
 
         return ret;
     }
 
     protected void onPostExecute(HashMap<String, String> myMap) {
-        String inputTxt = this.inputTxt;
-
-        // Update UI according with the given map
-        String outputTxt = myMap.get(inputTxt);
-
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, String.format("Mapping |%s| -> |%s|", inputTxt, outputTxt));
-        }
-
-        if (outputTxt == null) {
-            // Invalid translation
-            outputTxt = String.format("Input |%s| cannot be translated", inputTxt);
-            this.activity.showTranslateErrorDialog(outputTxt);
+        if (this.isServerError) {
+            this.activity.showTranslateErrorDialog(this.errorMessage);
         } else {
-            // Correct translation
-            TextView translateTxtView = (TextView)
-                    this.activity.findViewById(R.id.translated_txt_view);
-            translateTxtView.setText(outputTxt);
+            String inputTxt = this.inputTxt;
+
+            // Update UI according with the given map
+            String outputTxt = myMap.get(inputTxt);
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, String.format("Mapping |%s| -> |%s|", inputTxt, outputTxt));
+            }
+
+            if (outputTxt == null) {
+                // Invalid translation
+                outputTxt = String.format("Input |%s| cannot be translated", inputTxt);
+                this.activity.showTranslateErrorDialog(outputTxt);
+            } else {
+                // Correct translation
+                TextView translateTxtView = (TextView)
+                        this.activity.findViewById(R.id.translated_txt_view);
+                translateTxtView.setText(outputTxt);
+            }
         }
     }
 
