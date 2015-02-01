42af59c1ee00dbb70cc0fc8b34c286ece181aa25
Mond Wan <mondwan.1015@gmail.com>
2015-02-01 18:51:11 +0800

    Register a longClick handler on the ListVIew

--- app/src/main/java/com/iems5722/translateapp/ListTranslateRecord.java
+++ app/src/main/java/com/iems5722/translateapp/ListTranslateRecord.java
@@ -3,7 +3,10 @@ package com.iems5722.translateapp;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -21,16 +24,33 @@ public class ListTranslateRecord extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Get data from reading file
         ArrayList<String> items = this.readTranslationRecord();
 
+        // Bind those data to the adapter
         this.adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 items
         );
 
+        // Bind the adapter to our activity
         this.setListAdapter(this.adapter);
+
+        // Register a long click handler on our listview
+        ListView view = this.getListView();
+        view.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, String.format("Item pos |%d| id|%d|", position, id));
+                }
+
+                return true;
+            }
+        });
     }
+
 
     protected ArrayList<String> readTranslationRecord() {
         // Helper method for reading translation records
